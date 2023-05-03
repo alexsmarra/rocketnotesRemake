f3a8f8a2220586de47e549552ea4f6eafb980a46
Alexandre Marra <alexsmarra@hotmail.com>
2023-05-03 15:58:54 -0300

    Instalando library Styled Components para organizacao e processamento de nosso css, para escrever css dentro de js

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,10 @@
-import { Fragment } from "react"
-/* Quando importamos assim, automaticamente será aplicado o css em nossos elementos dessa página */
-import "./styles.css"
+import { Container } from "./styles"
 
 export function Details() {
   return (
-    /* Podemos utilizar o Fragment para embrulhar os elementos, mas ele (<Fragment>) não pode 
-    ser estilizado; caso precisemos de estilizar, podemos embrulhar os elementos com uma <div> 
-    comum mesmo. */
-    <Fragment>
+    <Container>
       <h1>Olá a todos!</h1>
       <span>Exemplo</span>
-    </Fragment>
+    </Container>
   )
 }
