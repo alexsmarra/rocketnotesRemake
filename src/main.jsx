8c3c16284da058529b38c97747773f1d3ddfb02f
Alexandre Marra <alexsmarra@hotmail.com>
2023-05-03 16:12:17 -0300

    importando ThemeProvider para prover nosso tema css para toda a aplicacao

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// para prover o nosso tema de styles/theme.js
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+
 import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Details />
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
