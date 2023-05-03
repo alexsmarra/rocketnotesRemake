ac55894319c5de216f4b03bb35c5b8af02b050ec
Alexandre Marra <alexsmarra@hotmail.com>
2023-05-03 13:03:16 -0300

    utilizando a tag Fragment

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,5 +1,13 @@
+import { Fragment } from "react"
+
 export function Details() {
   return (
-    <h1>Olá a todos!</h1>
+    /* Podemos utilizar o Fragment para embrulhar os elementos, mas ele (<Fragment>) não pode 
+    ser estilizado; caso precisemos de estilizar, podemos embrulhar os elementos com uma <div> 
+    comum mesmo. */
+    <Fragment>
+      <h1>Olá a todos!</h1>
+      <span>Exemplo</span>
+    </Fragment>
   )
 }
