afffd787ec8aeb0574f076bb250b8958b87e01a0
Alexandre Marra <alexsmarra@hotmail.com>
2023-05-04 09:52:45 -0300

    criando primeiro componente, Button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
-import { Container } from "./styles"
+import { Container } from './styles'
+
+import { Button } from '../../components/Button'
 
 export function Details() {
   return (
     <Container>
       <h1>Olá a todos!</h1>
       <span>Exemplo</span>
+
+      <Button />
     </Container>
   )
 }
